ba88ded6a5f8a05cad94f0a0673c21ccf4d7afb0
Riro Maclaurine Chacha <riro.chacha@student.moringaschool.com>
2024-01-20 01:05:53 +0300

    complete lab

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,9 @@
-def oxford_comma(items):
-    return None
+def oxford_comma(elements):
+    if len(elements) == 1:
+        return elements[0]
+    elif len(elements) == 2:
+        return "{} and {}".format(elements[0], elements[1])
+    else:
+        comma_separated = ", ".join(elements[:-1])
+        return "{}, and {}".format(comma_separated, elements[-1])
+    
